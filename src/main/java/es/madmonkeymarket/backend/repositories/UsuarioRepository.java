852e9e9bd689eac61e458f27cca24b2b6081e4ad
Miguel Catala Garrido <146656699+Miguel0549@users.noreply.github.com>
2025-10-27 22:21:12 +0100

    Delete src/main directory

--- src/main/java/es/madmonkeymarket/backend/repositories/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package es.madmonkeymarket.backend.repositories;
-import es.madmonkeymarket.backend.entities.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsuarioRepository extends JpaRepository<Usuario,String> {
-    boolean existsById(String id);
-}
