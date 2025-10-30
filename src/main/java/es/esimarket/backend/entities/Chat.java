24613f39b4d4650ced599fe75573c005d4751e8a
Julio Contioso Laínez <chaguiconti@gmail.com>
2025-10-30 16:31:29 +0100

    Entidad Chat

---
+++ src/main/java/es/esimarket/backend/entities/Chat.java
@@ -0,0 +1,53 @@
+package es.esimarket.backend.entities;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="chat")
+public class Chat{
+
+    @Id
+    @Column(name = "ID", nullable=false)
+    private String id;
+
+    @Column(name = "uDNI1", nullable=false)
+    private String uDNI1;
+
+    @Column(name = "uDNI2", nullable=false)
+    private String uDNI2;
+
+    @Column(name = "uDNI_Menor")
+    private String uDNI_Menor;
+
+    @Column(name = "uDNI_Mayor")
+    private String uDNI_Mayor;
+
+    public Chat() {}
+
+    public Chat(String id)
+    {
+        this.id = id;
+    }
+
+    public String getID() {return id;}
+    public void SetID(String id) {this.id = id;}
+
+    public String getuDNI1() {return uDNI1;}
+    public void setuDNI1(String uDNI1) {this.uDNI1=uDNI1;}
+
+    public String getuDNI2() {return uDNI2;}
+    public void setuDNI2(String uDNI2) {this.uDNI2=uDNI2;}
+
+    public String getuDNI_Menor() {return uDNI_Menor;}
+    public void setuDNI_Menor(String uDNI_Menor) {this.uDNI_Menor = uDNI_Menor;}
+
+    public String getuDNI_Mayor() {return uDNI_Mayor;}
+    public void setuDNI_Mayor(String uDNI_Mayor) {this.uDNI_Mayor = uDNI_Mayor;}
+
+
+
+
+
+}
