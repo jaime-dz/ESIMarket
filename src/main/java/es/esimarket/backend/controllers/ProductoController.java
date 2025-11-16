9e727f7833fb2ec12159bd6520990489aab52b5e
JulTrems34 <chaguiconti@gmail.com>
2025-11-16 19:07:35 +0100

    ProductoRequest

--- src/main/java/es/esimarket/backend/controllers/ProductoController.java
+++ src/main/java/es/esimarket/backend/controllers/ProductoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import es.esimarket.backend.controllers.requests.ProductoRequest;
 
 import java.util.List;
 
@@ -36,11 +37,14 @@ public class ProductoController {
     }
 
     @PostMapping("/")
-    public ResponseEntity<String> createProducto(@RequestParam String v, @RequestParam int p,
-                                                   @RequestParam String d, @RequestParam String n,
-                                                   @RequestParam String t, @RequestParam Producto.estado e)
+    public ResponseEntity<String> createProducto(@RequestBody final ProductoRequest request)
+    
+    //(@RequestParam String v, @RequestParam int p,
+    //@RequestParam String d, @RequestParam String n,
+    //@RequestParam String t, @RequestParam Producto.estado e,
+    //@RequestParam Producto.PagoAceptado pa)
     {
 
-        return productoService.nuevoProducto(v, p, d, n, t, e);
+        return productoService.nuevoProducto(request.uDNIVendedor(),request.precio(),request.descripcion(),request.Nombre(),request.Tipo(),request.estado(),request.pago());
     }
 }
---
+++ src/main/java/es/esimarket/backend/controllers/requests/ProductoRequest.java
@@ -0,0 +1,13 @@
+package es.esimarket.backend.controllers.requests;
+
+import es.esimarket.backend.entities.Producto;
+
+public record ProductoRequest(
+    String uDNIVendedor,
+    int precio,
+    String descripcion,
+    String Nombre,
+    String Tipo,
+    Producto.estado estado,
+    Producto.PagoAceptado pago 
+) {}
