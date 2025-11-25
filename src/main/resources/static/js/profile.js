f0de05fce8b657149e2f187e1a966b3a050df795
jaime-dz <jaimfercio@gmail.com>
2025-11-25 18:00:16 +0100

    Imagenes de perfil en colores, hecho js de perfil, modificado js de login, y creados html de perfil y editar perfil

---
+++ src/main/resources/static/js/profile.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    mostrarPerfil();
+});
+async function mostrarPerfil() {
+    const name=document.getElementById('show-name');
+    const surname=document.getElementById('show-surname');
+    const username=document.getElementById('show-username');
+    const email=document.getElementById('show-email');
+    const degree=document.getElementById('show-degree');
+    
+}
