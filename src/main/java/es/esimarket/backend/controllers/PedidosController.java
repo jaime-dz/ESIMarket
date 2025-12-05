f8d9488368c32e22aee45b99b3f3e17d19416885
JulTrems34 <chaguiconti@gmail.com>
2025-12-05 17:37:08 +0100

    Pedido terminado, falta lo de los token

--- src/main/java/es/esimarket/backend/controllers/PedidosController.java
+++ src/main/java/es/esimarket/backend/controllers/PedidosController.java
@@ -26,10 +26,28 @@ public class PedidosController{
         return ResponseEntity.ok(pedidosService.mostrar_pedidos());
     }
 
-    @GetMapping("/byuser")
-    public ResponseEntity<List<PedidosDTO>> getPedidosVendedor(String uDNI)
+    @GetMapping("/byseller")
+    public ResponseEntity<List<PedidosDTO>> getPedidosVendedor(@CookieValue(name = "refreshToken", required = false) String refreshToken, @CookieValue(name = "accessToken", required = false) String accessToken, String uDNI)
+    {
+        return ResponseEntity.ok(pedidosService.mostrar_pedidos_vendedor(uDNI));
+    }
+
+    @GetMapping("/bypurchaser")
+    public ResponseEntity<List<PedidosDTO>> getPedidosComprador(@CookieValue(name = "refreshToken", required = false) String refreshToken, @CookieValue(name = "accessToken", required = false) String accessToken, String uDNI)
     {
         return ResponseEntity.ok(pedidosService.mostrar_pedidos_comprador(uDNI));
+    }
+
+    @PatchMapping("/entregarpedido")
+    public ResponseEntity<String> patchPedidoVendedor(@CookieValue(name = "refreshToken", required = false) String refreshToken, @CookieValue(name = "accessToken", required = false) String accessToken, int IdPedido,int NTaquilla)
+    {
+        return ResponseEntity.ok(pedidosService.entregarPedido(IdPedido,NTaquilla));  //mirar si convviene mandarle el udni aunque no lo uses
+    }
+
+    @PatchMapping("/recogerpedido")
+    public ResponseEntity<String> patchPedidoComprador(@CookieValue(name = "refreshToken", required = false) String refreshToken, @CookieValue(name = "accessToken", required = false) String accessToken, int IdPedido)
+    {
+        return ResponseEntity.ok(pedidosService.recogerPedido(IdPedido));  //mirar si hay que enviar el udni aunque no lo uses
     }
 
     //El post de pedido va dentro de al funcion hacer compra
