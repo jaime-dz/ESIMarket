52c0f7ff414dbb92801466a86724d4fc72019b80
Miguel0549 <miguelcatalagarrido@gmail.com>
2026-01-12 23:35:42 +0100

    error sesion

--- src/main/java/es/esimarket/backend/config/JwtAuthFilter.java
+++ src/main/java/es/esimarket/backend/config/JwtAuthFilter.java
@@ -55,11 +55,13 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         // Obtener cookies de manera segura (maneja nulos)
         String accessToken = getCookieValue(request, "accessToken");
         String refreshToken = getCookieValue(request, "refreshToken");
+        String isLoggedIn = getCookieValue(request, "isLoggedIn");
 
         // ---------------------------------------------------------
         // CASO 1: Usuario sin credenciales (Cookies vacías)
         // ---------------------------------------------------------
         if (accessToken == null && refreshToken == null) {
+            limpiarCookies(response);
             if (esRutaPublica(path)) {
                 filterChain.doFilter(request, response);
                 return;
